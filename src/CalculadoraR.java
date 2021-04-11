7fb68594317ce0f56f05097b75940480c4b08c7b
Johnathan <Johnathan2@outlook.com.br>
2021-04-11 13:29:16 -0300

    Final da Aula01 Revisao

--- src/CalculadoraR.java
+++ src/CalculadoraR.java
@@ -1,27 +1,29 @@
 import java.util.Scanner;
 
-public class Teste4 {
+public class CalculadoraR {
 
 	public static void main(String[] args) {
 		Scanner leitor = new Scanner(System.in);
 		double primeiroValor;
 		double segundoValor;
-		double soma, subtracao, multiplicacao , divisao;
+		double soma , subtracao, multiplicacao , divisao ;
 		
-		System.out.println("Por favor, digita um valor");
+		
+		System.out.println("Por favor digite um valor");
 		primeiroValor = leitor.nextDouble();
-		System.out.println("Por favor, digita o segundo valor");
+		System.out.println("Por favor digite o segundo valor");
 		segundoValor = leitor.nextDouble();
 		soma = primeiroValor + segundoValor;
 		System.out.println("A soma entre os dois valores é " + soma);
 		subtracao = primeiroValor - segundoValor;
 		System.out.println("A soma entre os dois valores é " + subtracao);
-		multiplicacao =primeiroValor * segundoValor;
+		multiplicacao = primeiroValor * segundoValor;
 		System.out.println("A soma entre os dois valores é " + multiplicacao);
 		divisao = primeiroValor / segundoValor;
 		System.out.println("A soma entre os dois valores é " + divisao);
-	
+		
 		leitor.close();
+		
 	}
 
 }
